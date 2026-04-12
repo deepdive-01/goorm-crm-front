792ff0a77bae3fd7265e62f7532944d7fb2bc638
최유정 <cleame8922@naver.com>
2026-04-12 22:20:54 +0900

    [GOO-51] Signup 페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import ComponentExample from "./components/Example/ComponentExample";
+import Signup from "./pages/User/Auth/Signup";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<ComponentExample />} />
+      <Route path="/signup" element={<Signup />} />
     </Routes>
   );
 }
