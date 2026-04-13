d8df76d5bb6777ec1bde3b3b228416c27f7ca33d
최유정 <cleame8922@naver.com>
2026-04-13 19:52:34 +0900

    [GOO-36] 기타 example 및 package.json 수정

--- src/components/Example/InputExample.tsx
+++ src/components/Example/InputExample.tsx
@@ -3,56 +3,54 @@ import Input from "../common/Input/Input";
 
 export default function InputExample() {
   return (
-    <VStack $css={{ margin: "$200" }}>
-      <VStack
-        $css={{ gap: "$200" }}
-        className="w-fit p-7 border-[1px] border-gray-100 rounded-lg"
-      >
-        <Text className="text-h4">Input 컴포넌트</Text>
-        {/* 기본 input */}
-        <Input
-          name="required-field"
-          labelClassName="w-80"
-          validationMode="onChange"
-          label="필수 입력 항목"
-          placeholder="필수 입력 항목입니다"
-        />
-        <Input
-          name="optional-field"
-          labelClassName="w-80"
-          validationMode="onChange"
-          label="선택 입력 항목 (선택사항)"
-          placeholder="선택 입력 항목입니다"
-        />
-        <Input
-          name="required-field"
-          labelClassName="w-80"
-          validationMode="onChange"
-          label="비밀번호 (패턴 테스트)"
-          type="password"
-          match="patternMismatch"
-          pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W_]).{8,16}"
-          requiredError="8~16자, 대소문자 영문, 특수문자 포함"
-        />
-        <Input
-          name="optional-field"
-          labelClassName="w-80"
-          validationMode="onChange"
-          label="이메일"
-          description="이메일은 수정하실 수 없습니다."
-          defaultValue="kimminseok@example.com"
-          readonly
-        />
-        <Input
-          name="required-field"
-          labelClassName="w-80"
-          validationMode="onChange"
-          type="email"
-          match="patternMismatch"
-          label="이메일 (입력 값 테스트)"
-          requiredError="올바른 이메일 형식으로 작성해주세요."
-        />
-      </VStack>
+    <VStack
+      $css={{ gap: "$200" }}
+      className="w-fit p-7 border-[1px] border-gray-100 rounded-lg"
+    >
+      <Text className="text-h4">Input 컴포넌트</Text>
+      {/* 기본 input */}
+      <Input
+        name="required-field"
+        labelClassName="w-80"
+        validationMode="onChange"
+        label="필수 입력 항목"
+        placeholder="필수 입력 항목입니다"
+      />
+      <Input
+        name="optional-field"
+        labelClassName="w-80"
+        validationMode="onChange"
+        label="선택 입력 항목 (선택사항)"
+        placeholder="선택 입력 항목입니다"
+      />
+      <Input
+        name="required-field"
+        labelClassName="w-80"
+        validationMode="onChange"
+        label="비밀번호 (패턴 테스트)"
+        type="password"
+        match="patternMismatch"
+        pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W_]).{8,16}"
+        requiredError="8~16자, 대소문자 영문, 특수문자 포함"
+      />
+      <Input
+        name="optional-field"
+        labelClassName="w-80"
+        validationMode="onChange"
+        label="이메일"
+        description="이메일은 수정하실 수 없습니다."
+        defaultValue="kimminseok@example.com"
+        readonly
+      />
+      <Input
+        name="required-field"
+        labelClassName="w-80"
+        validationMode="onChange"
+        type="email"
+        match="patternMismatch"
+        label="이메일 (입력 값 테스트)"
+        requiredError="올바른 이메일 형식으로 작성해주세요."
+      />
     </VStack>
   );
 }
--- src/components/Example/TitleExample.tsx
+++ src/components/Example/TitleExample.tsx
@@ -3,28 +3,26 @@ import Title from "../common/Title/Title";
 
 export default function InputExample() {
   return (
-    <VStack $css={{ margin: "$200" }}>
-      <VStack
-        $css={{ gap: "$200" }}
-        className="w-fit p-7 border-[1px] border-gray-100 rounded-lg"
-      >
-        <Text className="text-h4">Title 컴포넌트</Text>
-        {/* 기본 title */}
-        <Title title="제목" describe="Describes" />
+    <VStack
+      $css={{ gap: "$200" }}
+      className="w-fit p-7 border-[1px] border-gray-100 rounded-lg"
+    >
+      <Text className="text-h4">Title 컴포넌트</Text>
+      {/* 기본 title */}
+      <Title title="제목" describe="Describes" />
 
-        {/* describe 색 조정 */}
-        <Title
-          title="최유정"
-          describe="goorm01@goorm.com"
-          DesClassName="text-gray-300"
-        />
+      {/* describe 색 조정 */}
+      <Title
+        title="최유정"
+        describe="goorm01@goorm.com"
+        DesClassName="text-gray-300"
+      />
 
-        {/* 관리자 페이지 예시 */}
-        <Title
-          title="대시보드"
-          describe="관리자 시스템의 주요 지표를 확인해보세요"
-        />
-      </VStack>
+      {/* 관리자 페이지 예시 */}
+      <Title
+        title="대시보드"
+        describe="관리자 시스템의 주요 지표를 확인해보세요"
+      />
     </VStack>
   );
 }
