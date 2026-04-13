a4018eddace93cf6599a434dcd59f46a909bb915
lee0806 <leeseh0806@gmail.com>
2026-04-13 13:01:14 +0900

    [GOO-40] 컴포넌트 테스트 삽입
    Signed-off-by: lee0806 <leeseh0806@gmail.com>

--- src/components/Example/ComponentExample.tsx
+++ src/components/Example/ComponentExample.tsx
@@ -1,12 +1,36 @@
 import { HStack } from "@vapor-ui/core";
 import InputExample from "./InputExample";
 import TitleExample from "./TitleExample";
+import Table from "../admin/Table/Table";
 
 export default function ComponentExample() {
   return (
     <HStack>
       <InputExample />
       <TitleExample />
+      <Table
+        variant="member"
+        data={[
+          {
+            id: 1,
+            name: "John Doe",
+            email: "john@example.com",
+            grade: "VIP",
+            status: "active",
+            joinDate: "2023-01-01",
+          },
+        ]}
+      />
+      <Table
+        variant="simple"
+        data={[
+          {
+            id: 1,
+            name: "John Doe",
+            email: "john@example.com",
+          },
+        ]}
+      />
     </HStack>
   );
 }
